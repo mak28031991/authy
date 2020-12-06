4ec6cc7628579b937f98ecd1a43e1394f27d1b73
mak28031991 <mak28031991@gmail.com>
2020-12-06 19:15:04 +0530

    updated library

--- src/main/java/com/airbnb/authy/service/auth/impl/AuthService.java
+++ src/main/java/com/airbnb/authy/service/auth/impl/AuthService.java
@@ -2,7 +2,9 @@ package com.airbnb.authy.service.auth.impl;
 
 import com.airbnb.authy.model.AuthRequest;
 import com.airbnb.authy.service.auth.IAuthService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService implements IAuthService {
 
     @Override
